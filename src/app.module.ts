f0d842751b472ee7e170ea8facca9749063362a5
Zak Mayfield <zakmayfield@gmail.com>
2023-05-11 11:33:24 -0400

    refactoring code, had a lot of redundant usage of modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { PrismaService } from './prisma.service';
 import { AppController } from './app.controller';
-
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
-
 import { BooksModule } from './books/books.module';
-import { BooksService } from './books/books.service';
-import { BooksController } from './books/books.controller';
 
 @Module({
   imports: [UsersModule, BooksModule],
-  controllers: [AppController, UsersController, BooksController],
-  providers: [PrismaService, UsersService, BooksService],
+  controllers: [AppController],
+  providers: [PrismaService],
 })
 export class AppModule {}
